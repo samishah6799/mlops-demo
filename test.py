edf017fccb04f3fcbb7ba94d45e9c6826b070d9f
samishah6799 <138593406+samishah6799@users.noreply.github.com>
2023-10-08 17:10:51 +0500

    10th Commit

--- test.py
+++ test.py
@@ -9,3 +9,7 @@ def test_bulb_status():
 def test_bulb_on():
     bulb.isOn()
     assert bulb.getStatus() == "Bulb is glowing"
+
+def test_bulb_off():
+    bulb.isOff()
+    assert bulb.getStatus() == "Bulb is not glowing"
