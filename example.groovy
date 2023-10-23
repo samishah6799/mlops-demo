cecccdea485c477d04a0a2e40a8e574abc37ab55
samishah6799 <138593406+samishah6799@users.noreply.github.com>
2023-10-23 09:58:10 +0500

    Create example.groovy

---
+++ example.groovy
@@ -0,0 +1,8 @@
+def deploy(String branch_name) {
+  if (branch_name == "PROD") {
+    println "Deploying to Production."
+  } else if (branch_name == "TEST") {
+    println "Deploying to Test."
+  } else {
+    println "Deploying to Dev."
+  }
