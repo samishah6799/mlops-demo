a93b536e4d9f1df7f61e448e64b3946982eeccbe
Shehryar Sohail <122811504+Shehryar718@users.noreply.github.com>
2023-10-25 01:33:41 +0500

    Update example.groovy

--- example.groovy
+++ example.groovy
@@ -1,12 +1,12 @@
-def deploy(String branch_name) {
-  if (branch_name == "PROD") {
-    println "Deploying to Production."
-  } else if (branch_name == "TEST") {
-    println "Deploying to Test."
-  } else {
-    println "Deploying to Dev."
-  }
-}
+// def deploy(String branch_name) {
+//   if (branch_name == "PROD") {
+//     println "Deploying to Production."
+//   } else if (branch_name == "TEST") {
+//     println "Deploying to Test."
+//   } else {
+//     println "Deploying to Dev."
+//   }
+// }
 
 def build() {
     println "Building"
