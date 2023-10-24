b7d2f098c22f1b4ac4f8a028c81e60e0bf324dfb
Shehryar Sohail <122811504+Shehryar718@users.noreply.github.com>
2023-10-25 01:44:37 +0500

    Update example.groovy

--- example.groovy
+++ example.groovy
@@ -8,10 +8,9 @@ def deploy(String branch_name) {
   }
 }
 
-// def build() {
-//   println "Building"
-//   // sh 'pip3 install -r requirements.txt'
-//   // Add build logic here
-// }
+def build() {
+  println "Building"
+  sh 'pip3 install -r requirements.txt'
+}
 
 return this
