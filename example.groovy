6b743f07977b07d808653387d06e0c54b5678a95
Shehryar Sohail <122811504+Shehryar718@users.noreply.github.com>
2023-10-25 01:46:52 +0500

    Update example.groovy

--- example.groovy
+++ example.groovy
@@ -1,14 +1,14 @@
-def deploy(String branch_name) {
-  if (branch_name == "PROD") {
-    println "Deploying to Production."
-  } else if (branch_name == "TEST") {
-    println "Deploying to Test."
-  } else {
-    println "Deploying to Dev."
-  }
-}
+// def deploy(String branch_name) {
+//   if (branch_name == "PROD") {
+//     println "Deploying to Production."
+//   } else if (branch_name == "TEST") {
+//     println "Deploying to Test."
+//   } else {
+//     println "Deploying to Dev."
+//   }
+// }
 
-def build(String branch_name) {
+def build() {
   println "Building"
   sh 'pip3 install -r requirements.txt'
 }
